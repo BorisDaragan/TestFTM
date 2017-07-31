55a4f573b9e69c457bcffae6c58829f7c6d07a90
BorisDaragan <boris.daragan@gmail.com>
2017-07-31 14:54:18 +0300

    1

---
+++ FTM.API/Controllers/PlayersController.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using FTM.API.Models;
+
+namespace FTM.API.Controllers
+{
+    public class PlayersController : ApiController
+    {
+        private FootballTeamsContext db = new FootballTeamsContext();
+
+        // GET: api/Players
+        public IQueryable<Player> GetPlayers()
+        {
+            return db.Players;
+        }
+
+        // GET: api/Players/5
+        [ResponseType(typeof(Player))]
+        public async Task<IHttpActionResult> GetPlayer(Guid id)
+        {
+            Player player = await db.Players.FindAsync(id);
+            if (player == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(player);
+        }
+
+        // PUT: api/Players/5
+        [ResponseType(typeof(void))]
+        public async Task<IHttpActionResult> PutPlayer(Guid id, Player player)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != player.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(player).State = EntityState.Modified;
+
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PlayerExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Players
+        [ResponseType(typeof(Player))]
+        public async Task<IHttpActionResult> PostPlayer(Player player)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Players.Add(player);
+
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (PlayerExists(player.Id))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtRoute("DefaultApi", new { id = player.Id }, player);
+        }
+
+        // DELETE: api/Players/5
+        [ResponseType(typeof(Player))]
+        public async Task<IHttpActionResult> DeletePlayer(Guid id)
+        {
+            Player player = await db.Players.FindAsync(id);
+            if (player == null)
+            {
+                return NotFound();
+            }
+
+            db.Players.Remove(player);
+            await db.SaveChangesAsync();
+
+            return Ok(player);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool PlayerExists(Guid id)
+        {
+            return db.Players.Count(e => e.Id == id) > 0;
+        }
+    }
+}
